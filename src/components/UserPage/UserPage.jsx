03383e1e5241f71705758b1e2e2b251692740633
Bennett Smrdel <smrdelb@gmail.com>
2022-01-28 11:07:38 -0600

    implemented the resume upload button, and ability to view the uploaded pdf file

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -3,6 +3,26 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 
 function UserPage() {
+  let fileURL;
+
+  const handleUpload = (event) => {
+    let file = event.target.files[0];
+    console.log(file);
+
+    if (file.type != 'application/pdf'){
+      console.error(file.name, 'is not a pdf file.')
+    } else {
+      console.log(file.name, 'Successful upload');
+      fileURL = URL.createObjectURL(file);
+    }
+  }
+
+  const handleSubmit = () => {
+    //Open the URL on new Window
+    const pdfWindow = window.open();
+    pdfWindow.location.href = fileURL; 
+  }
+
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
@@ -10,6 +30,13 @@ function UserPage() {
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
+
+      {/* These <br /> elements are temporary */}
+      <br /> 
+      <label htmlFor='resume-upload'>Upload Resume</label>
+      <input type='file' onChange={handleUpload} id='resume-upload' />
+      <br />
+      <button onClick={handleSubmit}>View Resume</button>
     </div>
   );
 }
