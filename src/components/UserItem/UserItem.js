29577985677acc06fba68cc97ef1bc848e1de8be
Caleb Hatch <hatchcaleb@gmail.com>
2022-01-28 14:52:03 -0600

    got header and about done on student page

--- src/components/UserItem/UserItem.js
+++ src/components/UserItem/UserItem.js
@@ -1,26 +1,40 @@
 import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 
 import "./UserItem.css";
 
 function UserItem({ dataItem }) {
   return (
     <div>
-      <div>
+      <div className="head">
         <div className="top">
           <img className="banner" src={dataItem.banner} />
         </div>
 
         <div className="sub">
-          <img className="picture" src={dataItem.picture} />
+          <Stack direction="row" spacing={2}>
+            <Avatar
+              className="avatar"
+              alt="Caleb"
+              src={dataItem.picture}
+              sx={{ width: 200, height: 200 }}
+            />
+          </Stack>
           <div>
             <div className="name-pros">
               <h2>{dataItem.name}</h2>
-              <p>{dataItem.pronouns}</p>
+              <p className="pronouns">{dataItem.pronouns}</p>
             </div>
-            <p>{dataItem.email}</p>
-            <p>{dataItem.linkedin}</p>
+            <p className="email">{dataItem.email}</p>
+            <p className="linked">{dataItem.linkedin}</p>
           </div>
         </div>
+      </div>
+
+      <div className="about">
+          <h3 className="about-text">About</h3>
+          <p className="about-data">{dataItem.about}</p>
       </div>
     </div>
   );
