62755bfaa7dead9dfb52acba6c6bccc43eb8c3d4
Bennett Smrdel <smrdelb@gmail.com>
2022-01-31 10:57:59 -0600

    data can populate the profile page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import UserItem from '../UserItem/UserItem';
 
 function UserPage() {
-  let fileURL;
-
-  const handleUpload = (event) => {
-    let file = event.target.files[0];
-    console.log(file);
-
-    if (file.type != 'application/pdf'){
-      console.error(file.name, 'is not a pdf file.')
-    } else {
-      console.log(file.name, 'Successful upload');
-      fileURL = URL.createObjectURL(file);
-    }
-  }
-
-  const handleSubmit = () => {
-    //Open the URL on new Window
-    const pdfWindow = window.open();
-    pdfWindow.location.href = fileURL; 
-  }
-
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const profile = useSelector((store) => store.profile);
-
 
   return (
     <div className="container">
