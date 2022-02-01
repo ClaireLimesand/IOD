cc59ae678b09924449728563e2d263abe738c6ca
Bennett Smrdel <smrdelb@gmail.com>
2022-02-01 11:48:58 -0600

    router can retrieve the profile picture and store it in the database

--- server/routes/picture.router.js
+++ server/routes/picture.router.js
@@ -9,37 +9,41 @@ require('dotenv').config();
 const cloudinary = require("cloudinary").v2;
 
 router.put('/', rejectUnauthenticated, (req, res) => {
-    const sqlText =
-    `
-        UPDATE "students" 
-        SET "picture" = $2
-        WHERE "user_id" = $1
-    `
-    const sqlValues = [
-        req.user.id,
-        req.body.picture
-    ];
+    let pictureUrl;
 
-     pool.query(sqlText, sqlValues)
-      .then((dbres) => {
-        res.sendStatus(201);
-
-        cloudinary.uploader
-        .upload(req.body.picture, {
-            resource_type: "image",
-        })
-        .then((result) => {
-            console.log('success', JSON.stringify(result, null, 2));
-        })
-        .catch((error) => {
-            console.log('error', JSON.stringify(error, null, 2));
-        });
+    cloudinary.uploader
+      .upload('./public/images/profile_pic.jpeg', {
+          resource_type: "image",
       })
-      .catch((dberror) => {
-        console.log('Oops you messed up DB error', dberror);
-        res.sendStatus(500)
-      })   
-    // endpoint functionality
+      .then((result) => {
+          console.log('success', JSON.stringify(result, null, 2));
+          pictureUrl = result.url;
+          console.log('URL!!!', pictureUrl);
+      })
+      .catch((error) => {
+          console.log('error', JSON.stringify(error, null, 2));
+      })
+      .then(() => {
+        const sqlText =
+        `
+            UPDATE "students" 
+            SET "picture" = $1
+            WHERE "user_id" = $2
+        `
+        const sqlValues = [
+            pictureUrl,
+            req.user.id
+        ];
+    
+         pool.query(sqlText, sqlValues)
+          .then((dbres) => {
+            res.sendStatus(201);
+          })
+          .catch((dberror) => {
+            console.log('Oops you messed up DB error', dberror);
+            res.sendStatus(500)
+          })  
+      })
   });
 
 module.exports = router;
