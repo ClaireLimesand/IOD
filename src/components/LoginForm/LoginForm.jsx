77aaa1448a9cc3fabe7c0341ea5b69c4313e026e
Bennett Smrdel <smrdelb@gmail.com>
2022-02-03 13:45:15 -0600

    copied css from register form, to login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -29,63 +29,67 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <img className="login-globe" src="globe.png" />
-      <center id="login-text">INTERNSHIP ON DEMAND</center>
-      <img className="login-gradient" src="gradient_bar.png" />
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div className="login-inputs">
-        <div>
-          <label htmlFor="username">
+    <div className="container">
+      <form className="formPanel" onSubmit={login}>
+        <center>
+          <img className="login-globe" src="globe.png" />
+          <center id="login-text">INTERNSHIP ON DEMAND</center>
+          <img className="login-gradient" src="gradient_bar.png" />
+          {errors.loginMessage && (
+            <h3 className="alert" role="alert">
+              {errors.loginMessage}
+            </h3>
+          )}
+          <div className="login-inputs">
+            <div>
+              <label htmlFor="username">
+                <input
+                  className="login-username"
+                  placeholder="Username"
+                  type="text"
+                  name="username"
+                  required
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="password">
+                <input
+                  className="login-password"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  required
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="login-main-button">
             <input
-              className="login-username"
-              placeholder="Username"
-              type="text"
-              name="username"
-              required
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
+              className="login-button"
+              type="submit"
+              name="submit"
+              value="Sign In"
             />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="password">
-            <input
-              className="login-password"
-              placeholder="Password"
-              type="password"
-              name="password"
-              required
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </label>
-        </div>
-      </div>
-      <div className="login-main-button">
-        <input
-          className="login-button"
-          type="submit"
-          name="submit"
-          value="Sign In"
-        />
-      </div>
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push("/registration");
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </form>
+          </div>
+          <center>
+            <button
+              type="button"
+              className="btn btn_asLink"
+              onClick={() => {
+                history.push("/registration");
+              }}
+            >
+              Register
+            </button>
+          </center>
+        </center>
+      </form>
+    </div>
   );
 }
 
