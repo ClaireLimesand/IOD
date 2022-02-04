12d9ce28c4ea499e6c1c3201d67499085fe17255
Claire Limesand <clairelimesand@gmial.com>
2022-02-04 13:09:44 -0600

    getting started on about page styling

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './AboutPage.css';
+
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -8,9 +10,11 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
-      <center>
-        <p>This about page is for anyone to read!</p>
-      </center>
+      <div className="about-header">
+        <center>
+          <p>This about page is for anyone to read!</p>
+        </center>
+      </div>
     </div>
   );
 }
