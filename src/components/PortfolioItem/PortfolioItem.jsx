f64773f36433f7714146ab5b528f55e7636721c8
Caleb Hatch <hatchcaleb@gmail.com>
2022-02-08 11:17:43 -0600

    finished styling on portfolio

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -9,10 +9,12 @@ function PortfolioItem({projects}) {
                 <img className="internship-logo" src={projects[0].company_logo} alt="Company Name"/>
                 {projects.map((project) => {
                     return (
-                        <div>
-                            <h2>{project.project_name}</h2>
-                            <img className="project-img" src={project.image} />
-                            <p>{project.description}</p>
+                        <div className="projects-sub">
+                            <h2 className="projects-name">{project.project_name}</h2>
+                            <div className="projects-info">
+                                <img className="project-img" src={project.image} />
+                                <p>{project.description}</p>
+                            </div>
                         </div>
                     )
                 })}
