5f10bbd71b8da47b586857c61119a62d20ecce7f
Bennett Smrdel <smrdelb@gmail.com>
2022-02-14 12:25:15 -0600

    created SelectedFavoriteProject componnent

---
+++ src/components/SelectedFavoriteProject/SelectedFavoriteProject.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useHistory } from "react-router";
+import { useDispatch, useSelector } from 'react-redux';
+import useReduxStore from '../../hooks/useReduxStore';
+import { useParams } from 'react-router-dom';
+
+function SelectedFavoriteProject() {
+    const favoriteProject = useSelector((store) => store.favoriteProject);
+
+    return (
+      favoriteProject.image != null ?
+        <div className="favProjects">
+          <h2 className="fav-name">{favoriteProject.project_name}</h2>
+          <div className="fav-info">
+            <img className="fav-img" src={favoriteProject.image} alt="Place for Image" />
+            <p>{favoriteProject.description}</p>
+          </div>
+        </div>
+      :
+        <div className="favProjects">
+          <h2 className="fav-name">Add a Favorite Project</h2>
+          <div className="fav-info">
+            <img className="fav-img" src={favoriteProject.image} />
+            <p>Go to your portfolio and press the star icon to favorite a project!</p>
+          </div>
+        </div>
+    );
+}
+
+export default SelectedFavoriteProject;
