edc38eb7083f360c4340c35893523e69ef05d20b
Bennett Smrdel <smrdelb@gmail.com>
2022-02-14 12:28:59 -0600

    favorited projects conditionally render on student load page

--- src/components/SelectedFavoriteProject/SelectedFavoriteProject.jsx
+++ src/components/SelectedFavoriteProject/SelectedFavoriteProject.jsx
@@ -8,22 +8,16 @@ function SelectedFavoriteProject() {
     const favoriteProject = useSelector((store) => store.favoriteProject);
 
     return (
-      favoriteProject.image != null ?
-        <div className="favProjects">
-          <h2 className="fav-name">{favoriteProject.project_name}</h2>
-          <div className="fav-info">
-            <img className="fav-img" src={favoriteProject.image} alt="Place for Image" />
-            <p>{favoriteProject.description}</p>
-          </div>
-        </div>
-      :
-        <div className="favProjects">
-          <h2 className="fav-name">Add a Favorite Project</h2>
-          <div className="fav-info">
-            <img className="fav-img" src={favoriteProject.image} />
-            <p>Go to your portfolio and press the star icon to favorite a project!</p>
-          </div>
-        </div>
+        favoriteProject.image != null ?
+            <div className="favProjects">
+            <h2 className="fav-name">{favoriteProject.project_name}</h2>
+            <div className="fav-info">
+                <img className="fav-img" src={favoriteProject.image} alt="Place for Image" />
+                <p>{favoriteProject.description}</p>
+            </div>
+            </div>
+        :
+            <div className="favProjects"></div>
     );
 }
 
--- src/components/SelectedProfilePage/SelectedProfilePage.jsx
+++ src/components/SelectedProfilePage/SelectedProfilePage.jsx
@@ -4,6 +4,7 @@ import SelectedProfileItem from '../SelectedProfilePage/SelectedProfileItem';
 import { useEffect } from 'react';
 import SelectedSkills from './SelectedSkills';
 import { useParams } from 'react-router';
+import SelectedFavoriteProject from '../SelectedFavoriteProject/SelectedFavoriteProject';
 
 function SelectedProfilePage() {
     // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -23,7 +24,10 @@ function SelectedProfilePage() {
             {profile.map((data) => {
                 return <SelectedProfileItem key={data.id} dataItem={data}/>
             })}
-            <SelectedSkills /> 
+            <div className="user-data">
+                <SelectedSkills /> 
+                <SelectedFavoriteProject />
+            </div>
         </div>
     );
 }
